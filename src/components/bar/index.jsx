38807275fb78b38af300d5f60949368cbfefabef
Killian Elie <killianelie@gmail.com>
2023-07-24 17:52:17 +0200

    ajout page 404

--- src/components/bar/index.jsx
+++ src/components/bar/index.jsx
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import arrow from './arrow_back_ios-24px 2.png';
-// import { useEffect} from 'react';
+import '../../styles/largebar.scss';
 
-function Bar(props) {
+function Collapse(props) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-  // function ClickOnArrow(){
-  //   const clickArrow = document.querySelector('.bar__titlebar__arrow');
+  const handleToggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  }
 
-  //   clickArrow.addEventListener('click', () => {
-      
-  //   })
-      
-  // }
-
-  // useEffect(() => {
-  //   ClickOnArrow();
-  // }, []); 
+  const collapseStyle = {
+    display: isCollapsed ? 'none' : 'block'
+  };
 
   return (
-    <div className='bar'>
-        <div className='bar__titlebar'>
-            <h2> {props.title} </h2> 
-            <img src={arrow} alt='flèche' className='bar__titlebar__arrow'/>
-        </div>
-
-        <p className='bar__textbar'> {props.content} </p>
+    <div className="bar">
+      <div className="bar__titlebar">
+        <h2>{props.title}</h2>
+        <button
+          className="bar__titlebar__button"
+          onClick={handleToggleCollapse}
+        >
+          {isCollapsed ? 
+            <img src={arrow} alt='flèche' className='bar__titlebar__button__toparrow'/> : <img src={arrow} alt='flèche' className='bar__titlebar__button__bottomarrow'
+          />}
+        </button>
+      </div>
+      <div className="bar__textbar" style={collapseStyle}>
+        <p className='bar__textbar__text'>{props.content}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Bar
+export default Collapse;
+
+
--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,9 +1,12 @@
 import '../../styles/index.scss';
+import { Link } from 'react-router-dom';
 
 function Error() {
   return (
-    <div>
-      <h1> 404</h1>
+    <div className='error'>
+      <p className='error__404'>404</p>
+      <p className='error__text'>Oups ! La page que vous avez demandée n'existe pas.</p>
+      <Link to="/" className='error__link'>Retour à la page d'accueil</Link>
     </div>
   )
 }
