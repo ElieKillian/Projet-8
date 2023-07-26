6ae5e63a2b68d8f306d26879ae7711b72c5778ad
Killian Elie <killianelie@gmail.com>
2023-07-26 16:43:33 +0200

    mise à jour header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,35 +1,30 @@
-import {useEffect} from 'react';
 import '../../styles/header.scss';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import logo from './logo.png';
  
 function Header() {
+  const location = useLocation();
 
-  function modifySelectline() {
-    const home = document.querySelector(".header__nav__home");
-    const about = document.querySelector(".header__nav__about");
-    
-    home.addEventListener('click', function(){
-      about.style.textDecoration = "none";
-      home.style.textDecoration = "underline";
+  useEffect(() => {
+    const currentUrl = location.pathname;
+    const menuLinks = document.querySelectorAll("a.header__nav__link");
+
+    menuLinks.forEach((link) => {
+      if (link.pathname === currentUrl) {
+        link.classList.add("active");
+      } else  {
+        link.classList.remove("active");
+      }
     });
-    
-    about.addEventListener('click', function(){
-      home.style.textDecoration = "none";
-      about.style.textDecoration = "underline";
-    })
-    }
-    
-    useEffect(() => {
-      modifySelectline();
-    }, []); 
+  }, [location]);
 
     return (
       <header className='header'>
         <h1 className='header__banner'> <img src={logo} alt="Logo" className='header__banner__img'/> </h1>
         <nav className='header__nav'>
-            <Link to="/" className='header__nav__home'>Accueil</Link>
-            <Link to="/a-propos" className='header__nav__about'>A propos</Link>
+            <Link to="/" className='header__nav__link'>Accueil</Link>
+            <Link to="/a-propos" className='header__nav__link'>A propos</Link>
         </nav>
       </header>
     )
